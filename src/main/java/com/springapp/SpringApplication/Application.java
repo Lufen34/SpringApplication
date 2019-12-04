bd08aeceb97fdfac216dc6b031573ad9f3cb018b
Lufen34 <lufen34@gmail.com>
2019-12-04 16:22:28 +0000

    + Exemple illustré pour mentor

--- src/main/java/com/springapp/SpringApplication/Application.java
+++ src/main/java/com/springapp/SpringApplication/Application.java
@@ -12,6 +12,10 @@ public class Application {
 	public static void main(String[] args) {
 		ConfigurableApplicationContext conf =  SpringApplication.run(Application.class, args);
 		Test t = conf.getBean(Test.class);
+		Test t2 = conf.getBean(Test.class);
 		t.start();
+		System.out.println(Test.getId());
+		t2.start();
+		System.out.println(Test.getId());
 	}
 }
--- src/main/java/com/springapp/SpringApplication/Test/Test.java
+++ src/main/java/com/springapp/SpringApplication/Test/Test.java
@@ -4,7 +4,24 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Test {
+    private static int id = 0;
+
+    public Test(){
+        // Voir avec le Mentor pourquoi cette ligne ne s'execute pas
+        System.out.println("Successfully called the Constructor.");
+        //id++;
+    }
+
     public void start() {
         System.out.println("Test successfully started.");
+        id++;
     }
+
+    /**
+     * @return the id
+     */
+    public static int getId() {
+        return id;
+    }
+
 }
